8d015b308c8a605a57fac7eee0351150009d8884
ISI-RCG-admin <skalicky@isi.edu>
2018-04-29 15:24:57 -0600

    added top level readme added library code

---
+++ lib/spyc.py
@@ -0,0 +1,91 @@
+# The Software is made available for academic or non-commercial purposes only. 
+# The license is for a copy of the program for an unlimited term. Individuals 
+# requesting a license for commercial use must pay for a commercial license. 
+# 
+# USC Stevens Institute for Innovation
+# University of Southern California
+# 1150 S. Olive Street, Suite 2300
+# Los Angeles, CA 90115, USA
+# ATTN: Accounting
+# 
+# DISCLAIMER. USC MAKES NO EXPRESS OR IMPLIED WARRANTIES, EITHER IN FACT OR 
+# BY OPERATION OF LAW, BY STATUTE OR OTHERWISE, AND USC SPECIFICALLY AND 
+# EXPRESSLY DISCLAIMS ANY EXPRESS OR IMPLIED WARRANTY OF MERCHANTABILITY OR 
+# FITNESS FOR A PARTICULAR PURPOSE, VALIDITY OF THE SOFTWARE OR ANY OTHER 
+# INTELLECTUAL PROPERTY RIGHTS OR NON-INFRINGEMENT OF THE INTELLECTUAL 
+# PROPERTY OR OTHER RIGHTS OF ANY THIRD PARTY. SOFTWARE IS MADE AVAILABLE 
+# AS-IS. LIMITATION OF LIABILITY.  TO THE MAXIMUM EXTENT PERMITTED BY LAW, 
+# IN NO EVENT WILL USC BE LIABLE TO ANY USER OF THIS CODE FOR ANY INCIDENTAL, 
+# CONSEQUENTIAL, EXEMPLARY OR PUNITIVE DAMAGES OF ANY KIND, LOST GOODWILL, 
+# LOST PROFITS, LOST BUSINESS AND/OR ANY INDIRECT ECONOMIC DAMAGES WHATSOEVER, 
+# REGARDLESS OF WHETHER SUCH DAMAGES ARISE FROM CLAIMS BASED UPON CONTRACT, 
+# NEGLIGENCE, TORT (INCLUDING STRICT LIABILITY OR OTHER LEGAL THEORY), A 
+# BREACH OF ANY WARRANTY OR TERM OF THIS AGREEMENT, AND REGARDLESS OF 
+# WHETHER USC WAS ADVISED OR HAD REASON TO KNOW OF THE POSSIBILITY OF 
+# INCURRING SUCH DAMAGES IN ADVANCE.
+# 
+# For commercial license pricing and annual commercial update and support pricing, please contact:
+# Rakesh Pandit
+# USC Stevens Institute for Innovation
+# University of Southern California
+# 1150 S. Olive Street, Suite 2300
+# Los Angeles, CA 90115, USA
+# Tel: +1 213-821-3552
+# Fax: +1 213-821-5001
+# Email: rakeshvp@usc.edu and CC to: accounting@stevens.usc.edu
+# 
+
+
+# Original Version
+# Author: Sam Skalicky, skalicky@isi.edu
+# Institution: Information Sciences Institute, University of Southern California
+# 
+
+
+#############################################################################################
+# spyc.py
+#
+# User-facing support library for contiguous memory management
+#
+#############################################################################################
+
+import pynq
+import numpy as np
+
+#create contiguous memory manager
+memmanager = pynq.xlnk.Xlnk()
+#create map of allocations for tracking
+allocated = {}
+
+#create a contiguous ndarray
+def init_contiguous_ndarray(shape=(32,32), dtype="float", buffer=None):
+    #calculate total number of elements
+    elements = 1
+    if isinstance(shape,tuple):
+        for a in shape:
+            elements *= a
+    else:
+        elements = shape
+    #calculate total bytes to allocate
+    length = elements * np.dtype(dtype).itemsize
+    #contiguous allocation
+    buffer_pointer = memmanager.cma_alloc(length,1,'char')
+    #create bufferable object with contiguous memory
+    buf = memmanager.cma_get_buffer(buffer_pointer, length)
+    #create ndarray
+    array = np.frombuffer(buf, dtype=dtype).reshape(shape)
+    #store allocation in map
+    allocated[str(array.data)] = buffer_pointer
+    #initialize memory if data is provided
+    if not(buffer is None):
+        np.copyto(array,buffer)
+        
+    return array
+
+#free a contiguous ndarray
+def free_contiguous_ndarray(arr):
+    #get contiguous memory address
+    buffer_pointer = allocated[str(arr.data)]
+    #free
+    memmanager.cma_free(buffer_pointer)
+            
---
+++ lib/spyc_dbg.cpp
@@ -0,0 +1,127 @@
+/*
+ * The Software is made available for academic or non-commercial purposes only. 
+ * The license is for a copy of the program for an unlimited term. Individuals 
+ * requesting a license for commercial use must pay for a commercial license. 
+ * 
+ * USC Stevens Institute for Innovation
+ * University of Southern California
+ * 1150 S. Olive Street, Suite 2300
+ * Los Angeles, CA 90115, USA
+ * ATTN: Accounting
+ * 
+ * DISCLAIMER. USC MAKES NO EXPRESS OR IMPLIED WARRANTIES, EITHER IN FACT OR 
+ * BY OPERATION OF LAW, BY STATUTE OR OTHERWISE, AND USC SPECIFICALLY AND 
+ * EXPRESSLY DISCLAIMS ANY EXPRESS OR IMPLIED WARRANTY OF MERCHANTABILITY OR 
+ * FITNESS FOR A PARTICULAR PURPOSE, VALIDITY OF THE SOFTWARE OR ANY OTHER 
+ * INTELLECTUAL PROPERTY RIGHTS OR NON-INFRINGEMENT OF THE INTELLECTUAL 
+ * PROPERTY OR OTHER RIGHTS OF ANY THIRD PARTY. SOFTWARE IS MADE AVAILABLE 
+ * AS-IS. LIMITATION OF LIABILITY.  TO THE MAXIMUM EXTENT PERMITTED BY LAW, 
+ * IN NO EVENT WILL USC BE LIABLE TO ANY USER OF THIS CODE FOR ANY INCIDENTAL, 
+ * CONSEQUENTIAL, EXEMPLARY OR PUNITIVE DAMAGES OF ANY KIND, LOST GOODWILL, 
+ * LOST PROFITS, LOST BUSINESS AND/OR ANY INDIRECT ECONOMIC DAMAGES WHATSOEVER, 
+ * REGARDLESS OF WHETHER SUCH DAMAGES ARISE FROM CLAIMS BASED UPON CONTRACT, 
+ * NEGLIGENCE, TORT (INCLUDING STRICT LIABILITY OR OTHER LEGAL THEORY), A 
+ * BREACH OF ANY WARRANTY OR TERM OF THIS AGREEMENT, AND REGARDLESS OF 
+ * WHETHER USC WAS ADVISED OR HAD REASON TO KNOW OF THE POSSIBILITY OF 
+ * INCURRING SUCH DAMAGES IN ADVANCE.
+ * 
+ * For commercial license pricing and annual commercial update and support pricing, please contact:
+ * Rakesh Pandit
+ * USC Stevens Institute for Innovation
+ * University of Southern California
+ * 1150 S. Olive Street, Suite 2300
+ * Los Angeles, CA 90115, USA
+ * Tel: +1 213-821-3552
+ * Fax: +1 213-821-5001
+ * Email: rakeshvp@usc.edu and CC to: accounting@stevens.usc.edu
+ * 
+ */
+
+
+/*
+ * Original Version
+ * Author: Sam Skalicky, skalicky@isi.edu
+ * Institution: Information Sciences Institute, University of Southern California
+ * 
+ */
+
+
+/********************************************
+ * spyc_dbg.cpp
+ * DEBUG C/C++ library of functions for traversing
+ *   the Python<-->C/C++ boundary
+ ********************************************/
+
+#include <Python.h>
+#include <numpy/arrayobject.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "spyc.h"
+#include <stdio.h>
+
+
+/* cleanupArrayArg
+ *   Cleanup after C/C++ program before returning to Python
+ */
+void cleanupArrayArg(PyArrayObject* obj, void** ptr) {
+  //debug print
+  printf("cleanupArrayArg\n");
+  printf("\n\tptr: %p\n",*ptr);
+
+  //do nothing 
+}
+
+
+/* setupArrayArg
+ *    Setup for C/C++ program
+ */
+void setupArrayArg(PyArrayObject* obj, void** ptr, int type, char* str) {
+  //debug print
+  printf("setupArrayArg\n");
+  printf("\ttype: ");
+  printType(type);
+  printf("\targ: %s\n",str);
+  
+  if(PyArray_TYPE(obj) != type) {
+    printf("Error! Expected array of '");
+    printType(type);
+    printf("' for argument %s but found '",str);
+    printType(PyArray_TYPE(obj));
+    printf("'\n");
+  }
+  else
+    *ptr = PyArray_DATA(obj);
+  
+  //debug print
+  printf("\n\tptr: %p\n",*ptr);
+}
+
+/* printType
+ *    print the integer type enum as a string
+ */
+void printType(int type) {
+  switch(type) {
+  case NPY_INT8: printf("int8");
+    break;
+  case NPY_INT16: printf("int16");
+    break;
+  case NPY_INT32: printf("int32");
+    break;
+  case NPY_INT64: printf("int64");
+    break;
+  case NPY_UINT8: printf("uint8");
+    break;
+  case NPY_UINT16: printf("uint16");
+    break;
+  case NPY_UINT32: printf("uint32");
+    break;
+  case NPY_UINT64: printf("uint64");
+    break;
+  case NPY_FLOAT: printf("float");
+    break;
+  case NPY_DOUBLE: printf("double");
+    break;
+
+  default: printf("ERR/UNKN");
+  }
+}
