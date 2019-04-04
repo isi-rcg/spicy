fb9d8e08ca7f7dda231d4595eb0c28c0e4e78de6
samskalicky <samskalicky@gmail.com>
2019-04-04 02:30:40 +0000

    added initial mxnet example

--- examples/mxnet/test.py
+++ examples/mxnet/test.py
@@ -3,9 +3,9 @@ import mxnet as mx
 import time
 
 # Function to be translated (function under test)
-def mmult(a:np.ndarray((2,2),'float32'), 
-          b:np.ndarray((2,2),'float32'), 
-          c:np.ndarray((2,2),'float32')):
+def mmult(a:np.ndarray((32,32),'float32'), 
+          b:np.ndarray((32,32),'float32'), 
+          c:np.ndarray((32,32),'float32')):
     # Function interface pragmas
     '''
     #pragma SDS data access_pattern(a:SEQUENTIAL, b:SEQUENTIAL, c:SEQUENTIAL)
@@ -13,9 +13,13 @@ def mmult(a:np.ndarray((2,2),'float32'),
     #pragma SDS data data_mover(a:AXIDMA_SIMPLE, b:AXIDMA_SIMPLE, c:AXIDMA_SIMPLE)
     '''
     # internal partitioned arrays
-    A_tmp = np.ndarray((2,2),'float32')
-    B_tmp = np.ndarray((2,2),'float32')
+    A_tmp = np.ndarray((32,32),'float32')
+    B_tmp = np.ndarray((32,32),'float32')
 
+    '''
+    #pragma HLS array_partition variable=A_tmp cyclic factor=16 dim=1
+    #pragma HLS array_partition variable=B_tmp block factor=16 dim=1
+    '''
     # Copy loops
     for i in range(a.shape[0]):
         for j in range(a.shape[1]):
@@ -38,9 +42,9 @@ def mmult(a:np.ndarray((2,2),'float32'),
 
 class Gemm(mx.operator.CustomOp):
     def forward(self, is_train, req, in_data, out_data, aux):
-        A = np.ndarray((2,2),dtype='float32',buffer=in_data[0].asnumpy())
-        B = np.ndarray((2,2),dtype='float32',buffer=in_data[1].asnumpy())
-        C = np.ndarray((2,2),dtype='float32',buffer=out_data[0].asnumpy())
+        A = np.ndarray((32,32),dtype='float32',buffer=in_data[0].asnumpy())
+        B = np.ndarray((32,32),dtype='float32',buffer=in_data[1].asnumpy())
+        C = np.ndarray((32,32),dtype='float32',buffer=out_data[0].asnumpy())
         mmult(A,B,C)
         self.assign(out_data[0], req[0], mx.nd.array(C))
 
@@ -67,11 +71,11 @@ class GemmProp(mx.operator.CustomOpProp):
     def create_operator(self, ctx, in_shapes, in_dtypes):
         return Gemm()
 
-A = mx.nd.array([[1, 2], [3, 4]])
-B = mx.nd.array([[5, 6], [7, 8]])
-start = time.time()
-C = mx.nd.Custom(A, B, op_type='gemm')
-mx.nd.waitall()
-stop = time.time()
-print(C)
-print("%s" % (stop-start))
+A = mx.nd.array(np.linspace(1,1024,1024,dtype='float32').reshape(32,32))
+B = mx.nd.array(np.linspace(1,1024,1024,dtype='float32').reshape(32,32))
+for i in range(10):
+    start = time.time()
+    C = mx.nd.Custom(A, B, op_type='gemm')
+    mx.nd.waitall()
+    stop = time.time()
+    print("elapsed: %4.2f ms" % ((stop-start)*1000))
